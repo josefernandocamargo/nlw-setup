26677de9d56b01078177b05687e010b4e26bc93b
josefernandocamargo <josefernando.cam@gmail.com>
2023-01-22 00:53:07 -0300

    input apenas do dia

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
-    //const today = new Date().toLocaleDateString("pt-br").slice(0,-5)
-    const today = "3/01"
+    const today = new Date().toLocaleDateString("pt-br").slice(0,-5)
+    //const today = "3/01" para testes, input manual
     const dayExists = nlwSetup.dayExists(today)
 
     if (dayExists) {
